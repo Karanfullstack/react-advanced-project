87581cd628e95f419b654417e5ad2d7404cc06de
Karan <karan@karans-air.home>
2023-05-11 20:20:45 +0200

    setting up project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {fetchDataFromApi} from "./utils/api";
+import {useDispatch, useSelector} from "react-redux";
+import {getApiConfiguration} from "./store/homeSlice";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/*<Header />*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.scss";
+
+const HeroBanner = () => {
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBanner">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover Explore now
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
