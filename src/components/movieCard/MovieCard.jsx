3f097f857473774fd680d1277aa72f7724e7b7b7
Karan <karan@karans-air.home>
2023-06-03 21:01:14 +0200

    final touch

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import dayjs from "dayjs";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
-import {Img} from "../lazyLoaderImage/Img";
+import {Img} from "../../components/lazyLoaderImage/Img";
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
